73a18e17c70e0653ab20df5c5f3b9906e2aaec52
hahaahee <93078014+hahaahee@users.noreply.github.com>
2022-09-01 13:44:24 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi6.py",
+    version = "0.1",
+    py_modules = ['agi6'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
